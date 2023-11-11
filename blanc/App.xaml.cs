b881b9f12a40c4c3930806f439a7457cd5b259de
VikoPiko <vikoasmatiko@gmail.com>
2023-11-11 15:58:54 +0200

    Add project files.

---
+++ blanc/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using blanc.Views;
+
+namespace blanc
+{
+    public partial class App : Application
+    {
+        
+    }
+}
