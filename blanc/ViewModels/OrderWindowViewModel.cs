42f1c119f9a2880191207075c7d74146c163cfa2
VikoPiko <vikoasmatiko@gmail.com>
2023-12-10 22:15:30 +0200

    Kitchen i OrderWindow- Opit za rabotesht kod?

---
+++ blanc/ViewModels/OrderWindowViewModel.cs
@@ -0,0 +1,81 @@
+﻿using blanc.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace blanc.ViewModels
+{
+    public  class OrderWindowViewModel : ViewModelBase
+    {
+        const string billJsn = "Bill.json";
+
+        private Bill _orderSelected;
+        public Bill OrderSelected
+        {
+            get => _orderSelected;
+            set
+            {
+                _orderSelected = value;
+                OnPropertyChanged(nameof(OrderSelected));
+            }
+        }
+        private Orders _items;// honestly ne znam dali mi trqbva tva :"D
+        public Orders Items 
+        {
+            get => _items;
+            set 
+            {
+                _items = value; OnPropertyChanged(nameof(Items));
+            }
+        } // honestly ne znam dali mi trqbva tva :"D
+
+        private ObservableCollection<Bill>? _billItems;
+        public ObservableCollection<Bill>? BillItems // used to store the bill items which have been ordered; mai? 
+        {
+            get => _billItems;
+            set { _billItems = value; OnPropertyChanged(nameof(BillItems)); }
+        }
+
+        private ObservableCollection<Orders>? _itemsFromOrder; // honestly ne znam dali mi trqbva tva :"D
+        public ObservableCollection<Orders>? ItemsFromOrder 
+        {
+            get => _itemsFromOrder;
+            set 
+            {
+                _itemsFromOrder = value; OnPropertyChanged(nameof(ItemsFromOrder));
+            }
+        }// honestly ne znam dali mi trqbva tva :"D
+
+        public OrderWindowViewModel() 
+        {
+            LoadOrders();
+        }
+
+        private void LoadOrders() // Loadva Itemite koito sa poruchani i sa v bill; Shte go polzvam za zarejdane na itemite v Orders
+        {
+            try
+            {
+                string billJson = File.ReadAllText(billJsn);
+                List<Bill> billItems = JsonConvert.DeserializeObject<List<Bill>>(billJsn);
+                BillItems = new ObservableCollection<Bill>(billItems);
+                OnPropertyChanged(nameof(BillItems));
+            }
+            catch (FileNotFoundException ex)
+            {
+                Debug.WriteLine("Файлът не може да бъде намерен: " + ex.FileName);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Възникна грешка при зареждане на менюто: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ blanc/Views/OrderWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using blanc.Models;
+using blanc.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace blanc.Views
+{
+    public partial class OrderWindow : Window
+    {
+
+        private KitchenView _mainWindowKitchen;
+        const string billJsn = "Bill.json";
+        public OrderWindow()
+        {
+            try
+            {
+                InitializeComponent();
+                MiniTableViewModel viewModel = new MiniTableViewModel();
+
+                this.DataContext = viewModel;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while opening the order XDD.");
+            }
+        }
+        public OrderWindow(Orders selectedOrder) : this()
+        {
+            var viewModel = this.DataContext as OrderWindowViewModel;
+            if (viewModel != null)
+            {
+                //viewModel.SelectedOrder = selectedOrder;
+            }
+        }
+
+        private void OrdersClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            OrderWindowViewModel? viewModel = this.DataContext as OrderWindowViewModel;
+            if (viewModel != null) 
+            {
+                int currentOrderId = viewModel.OrderSelected.tableId;
+                string orderData = File.ReadAllText(billJsn);
+                var allOrders = JsonConvert.DeserializeObject<List<Orders>>(orderData) ?? new List<Orders>();
+                var tableOrders = allOrders.Where(b => b.OrderID == currentOrderId);
+                viewModel.ItemsFromOrder = new ObservableCollection<Orders>(tableOrders);
+            }
+        }
+    }
+}
